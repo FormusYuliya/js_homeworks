af165434a5fd4df4bf351a96f73fd75098561346
FormusYuliya <formus.yuliya@gmail.com>
2024-03-11 22:23:25 +0200

    Finally complete second task

--- js_homework5.js
+++ js_homework5.js
@@ -1,3 +1,11 @@
+/*ДЗ1 
+
+Створіть об'єкт з щонайменше трьома властивостями (намагайтесь виявити фантазію). 
+Напишіть метод getInfo(), який виводитиме всі властивості об'єкта у вигляді пар "ключ: значення". 
+Зверніть увагу: метод повинен працювати навіть після додавання нових властивостей до об'єкта. 
+
+*/
+
 let brains = {
     weight: "1.4 кг",
     percentageOfBurnedNeurons: 50,
@@ -17,3 +25,54 @@ let brains = {
   brains.learning = 'Вивчаемо JS!';
 
   brains.getInfo();
+
+  // ДЗ2 Сума вартості послуг
+
+    var services = {
+    "стрижка": "60 грн",
+    "гоління": "80 грн",
+    "Миття голови": "100 грн",
+  
+
+    price: function() {
+        let total = 0;
+        for (let key in this) {
+            if (typeof this[key] !== 'function'){
+                total += +this[key].replace(' грн', '');
+            }
+        }
+        return total;
+    },
+
+    minPrice: function() {
+        let min = Infinity;
+        for (let key in this) {
+            if (typeof this[key] !== 'function') {
+                let price = this[key].replace(' грн', '');
+                min = Math.min(min, price);
+            }
+        }
+        return min;
+    },
+
+
+    maxPrice: function() {
+        let max = 0;
+        for (let key in this) {
+            if(typeof this[key] !== 'function') {
+                let price = this[key].replace(' грн', '');
+                max = Math.max(max, price);
+            }
+        }
+        return max;
+    }
+};
+
+
+services['Розбити скло'] = "200 грн";
+
+
+console.log("Загальна вартість послуг: " + services.price() + " грн");
+console.log("Мінімальна вартість: " + services.minPrice() + " грн");
+console.log("Максимальна вартість: " + services.maxPrice() + " грн");
+
